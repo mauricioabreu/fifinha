e11d31af7f02d1510bdb86409291b85c9d4e5ecb
Maurício Antunes <mauricio.abreua@gmail.com>
2019-07-09 00:44:32 -0300

    Bootstrap bot

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+	bot.Debug = true
+
+	log.Printf("Authorized on account: %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			switch update.Message.Command() {
+			case "help":
+				msg.Text = "Type /jogo"
+			default:
+				msg.Text = "Unknown command"
+			}
+			bot.Send(msg)
+		}
+	}
+}
